c101f092f6b733d1d378343645de0bc50ece93ee
Carlos <nicolaugalves@gmail.com>
2026-02-08 14:47:52 -0300

    ajustando tela e dando um exemplo de module produto para cadastro

--- MakeLifeBetter/composeApp/src/commonMain/kotlin/com/carlosnicolaugalves/makelifebetter/screens/event/store/CheckoutScreen.kt
+++ MakeLifeBetter/composeApp/src/commonMain/kotlin/com/carlosnicolaugalves/makelifebetter/screens/event/store/CheckoutScreen.kt
@@ -4,11 +4,14 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
@@ -113,7 +116,7 @@ fun CheckoutScreen(
         },
         bottomBar = {
             Surface(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier.fillMaxWidth().windowInsetsPadding(WindowInsets.navigationBars),
                 shadowElevation = 8.dp,
                 color = MaterialTheme.colorScheme.surface
             ) {
--- MakeLifeBetter/composeApp/src/commonMain/kotlin/com/carlosnicolaugalves/makelifebetter/screens/event/store/ProductDetailScreen.kt
+++ MakeLifeBetter/composeApp/src/commonMain/kotlin/com/carlosnicolaugalves/makelifebetter/screens/event/store/ProductDetailScreen.kt
@@ -7,13 +7,16 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -80,7 +83,9 @@ fun ProductDetailScreen(
         },
         bottomBar = {
             Surface(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .windowInsetsPadding(WindowInsets.navigationBars),
                 shadowElevation = 8.dp,
                 color = MaterialTheme.colorScheme.surface
             ) {
