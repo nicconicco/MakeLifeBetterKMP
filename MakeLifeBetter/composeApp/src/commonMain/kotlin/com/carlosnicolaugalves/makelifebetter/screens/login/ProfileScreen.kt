94510cc809528d6397588976faa4887c7cfa729c
Carlos <nicolaugalves@gmail.com>
2026-02-10 10:53:50 -0300

    Profile ok

--- MakeLifeBetter/composeApp/src/commonMain/kotlin/com/carlosnicolaugalves/makelifebetter/screens/login/ProfileScreen.kt
+++ MakeLifeBetter/composeApp/src/commonMain/kotlin/com/carlosnicolaugalves/makelifebetter/screens/login/ProfileScreen.kt
@@ -7,12 +7,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
@@ -388,7 +388,6 @@ fun ProfileScreen(
                             )
                         }
                     },
-                    windowInsets = WindowInsets(0, 0, 0, 0),
                     colors = TopAppBarDefaults.topAppBarColors(
                         containerColor = MaterialTheme.colorScheme.surface
                     )
@@ -399,7 +398,7 @@ fun ProfileScreen(
             content(Modifier.padding(paddingValues))
         }
     } else {
-        content(modifier)
+        content(modifier.statusBarsPadding())
     }
 
     // Secret password dialog
